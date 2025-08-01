2fd00c68924bfa9ff61e785d2563185f4cd1215d
Felixix L_X <a.felipemg16@gmail.com>
2025-08-01 13:30:25 -0500

    verión 1.51

--- static/js/login.js
+++ static/js/login.js
@@ -3,20 +3,15 @@ function mostrarFormulario(rol) {
   document.getElementById('login-box').style.display = 'block';
   document.getElementById('rolInput').value = rol;
 
-  // Mostrar título según rol
   const titulo = document.getElementById('titulo-formulario');
   titulo.textContent = rol === 'admin' ? 'Ingreso Administrador' : 'Ingreso Usuario';
 
-  // Mostrar o no el enlace de registro
   const registro = document.getElementById('registro-link');
-  if (rol === 'usuario') {
-    registro.style.display = 'block';
-  } else {
-    registro.style.display = 'none';
-  }
+  registro.style.display = rol === 'usuario' ? 'block' : 'none';
 }
 
 function volverSelector() {
   document.getElementById('selector').style.display = 'block';
   document.getElementById('login-box').style.display = 'none';
+  document.getElementById('rolInput').value = '';
 }
